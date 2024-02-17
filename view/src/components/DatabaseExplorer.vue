a8de4907b07210a2d3387942fc5dafa03283de62
Benjamin DERVILLE <benderville@gmail.com>
2024-02-18 00:34:57 +0100

    Add database explorer filter and used indication

--- view/src/components/DatabaseExplorer.vue
+++ view/src/components/DatabaseExplorer.vue
@@ -7,11 +7,25 @@
       <div class="px-4 py-2" v-if="tables?.length === 0">
         No table available. <br />Please contact support.
       </div>
-      <li v-for="(table, ind) in tables" @click="onClick(ind)" v-on:dblclick="onDblClick(ind)">
-        <div href="#" class="block hover:bg-gray-50">
+      <input
+        type="text"
+        placeholder="search table"
+        class="block w-full max-w-lg border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+        id="searchTables"
+        v-model="searchTablesInput"
+      />
+      <li
+        v-for="(table, ind) in filteredTables"
+        @click="onClick(ind)"
+        v-on:dblclick="onDblClick(ind)"
+      >
+        <div class="block hover:bg-gray-50">
           <div class="px-4 py-4 sm:px-6">
             <div class="flex items-center justify-between">
-              <div class="text-sm font-medium text-blue-600 truncate">
+              <div
+                class="text-sm font-medium truncate"
+                :class="isTableUsed(table) ? 'text-blue-600' : 'text-gray-600'"
+              >
                 {{ table.schema }}.{{ table.name }}
               </div>
             </div>
@@ -34,12 +48,15 @@
           </div>
         </div>
       </li>
+      <div v-if="filteredTables.length == 0" class="block hover:bg-gray-50">
+        <p class="px-4 py-4 sm:px-6">No tables</p>
+      </div>
     </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue'
+import { ref, watchEffect, computed } from 'vue'
 import { useDatabases } from '../stores/databases'
 import { querySQL, runQuery } from '../stores/query'
 
@@ -55,30 +72,88 @@ interface Table {
   schema: string
   description: string
   columns: Column[]
+  used: Boolean // information if the table is used in the query
 }
 
-const showTableIndex = ref(0)
+const showTableIndex = ref(null)
+const searchTablesInput = ref('')
 const tables = ref<Table[]>([])
 
+function extractTables(sqlQuery) {
+  // Regular expression to match table names following FROM, JOIN, and UPDATE keywords
+  // This regex is basic and might need adjustments to cover all SQL syntax variations
+  const regex = /\b(FROM|JOIN|UPDATE|INTO)\s+("?\w+"?\."?\w+"?|"\w+"|\w+)/gi
+
+  let match
+  const extables = []
+
+  // Use a loop to find matches and push the table name to the tables array
+  while ((match = regex.exec(sqlQuery)) !== null) {
+    // This ensures the match was not empty or undefined
+    if (match[2]) {
+      extables.push(match[2].replaceAll('"', ''))
+    }
+  }
+
+  // Remove duplicates by converting to a Set and back to an Array
+  return [...new Set(extables)]
+}
+
+const extractedTables = computed(() => {
+  return extractTables(querySQL.value)
+})
+
+const isTableUsed = (table: Table) => {
+  for (const extractedTable of extractedTables.value) {
+    if (extractedTable.includes('.')) {
+      const [schema, name] = extractedTable.split('.')
+      if (table.schema === schema && table.name === name) {
+        return true
+      }
+    } else if (table.name === extractedTable) {
+      return true
+    }
+  }
+  return false
+}
+
+const sortedTables = computed(() => {
+  // make a copy of the tables array and sort it by the used property
+  return [...tables.value].sort((a, b) => {
+    if (isTableUsed(a) && !isTableUsed(b)) {
+      return -1
+    }
+    if (!isTableUsed(a) && isTableUsed(b)) {
+      return 1
+    }
+    return 0
+  })
+})
+
+const filteredTables = computed(() => {
+  return sortedTables.value.filter((table: Table) => {
+    return table.name.includes(searchTablesInput.value)
+  })
+})
+
 const onClick = (tableInd: number) => {
-  showTableIndex.value = tableInd
+  if (showTableIndex.value == tableInd) {
+    showTableIndex.value = null
+  } else {
+    showTableIndex.value = tableInd
+  }
 }
 
 const onDblClick = (tableInd: number) => {
-  console.log(databaseSelectedId)
   const tableSelected = tables.value[tableInd]
-  console.log(tableSelected)
-
   querySQL.value = `SELECT * FROM "${tableSelected.schema}"."${tableSelected.name}";`
   runQuery()
 }
 
 watchEffect(async () => {
-  console.log('databaseSelected', databaseSelectedId.value)
   const selectedDatabaseId = databaseSelectedId.value
   if (selectedDatabaseId) {
     tables.value = await fetchDatabaseTables(selectedDatabaseId)
-    console.log('tables', tables.value)
   }
 })
 </script>
