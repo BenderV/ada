b3a921586187d0d4f1a06da0ca71c33893af58fd
Benjamin DERVILLE <benderville@gmail.com>
2024-05-18 10:19:01 +0200

    clean and mini fix

--- service/chat/utils_test.py
+++ service/chat/utils_test.py
@@ -1,38 +1,7 @@
 import unittest
 
 from chat.memory_utils import find_closest_embeddings
-from chat.utils import message_replace_json_block_to_csv, parse_function
-
-
-class TestMessageReplaceJsonBlockToCsv(unittest.TestCase):
-    def test_single_json_block(self):
-        input_content = 'Hello, here is some data: ```json\n[{"key": "value"}]\n```'
-        expected_output = "Hello, here is some data: ```csv\nkey\nvalue\n```"
-
-        output_content = message_replace_json_block_to_csv(input_content)
-        self.assertEqual(output_content, expected_output)
-
-    def test_multiple_json_blocks(self):
-        input_content = (
-            "Hello, here are multiple blocks:\n"
-            'First: ```json\n[{"key1": "value1"}]\n```\n'
-            'Second: ```json\n[{"key2": "value2"}]\n```'
-        )
-        expected_output = (
-            "Hello, here are multiple blocks:\n"
-            "First: ```csv\nkey1\nvalue1\n```\n"
-            "Second: ```csv\nkey2\nvalue2\n```"
-        )
-
-        output_content = message_replace_json_block_to_csv(input_content)
-        self.assertEqual(output_content, expected_output)
-
-    def test_no_json_blocks(self):
-        input_content = "Hello, there are no JSON blocks in this message."
-        expected_output = "Hello, there are no JSON blocks in this message."
-
-        output_content = message_replace_json_block_to_csv(input_content)
-        self.assertEqual(output_content, expected_output)
+from chat.utils import parse_function
 
 
 class TestDatabaseChat(unittest.TestCase):
